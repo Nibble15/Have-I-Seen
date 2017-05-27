1c5776b80856db05e60026bdd541d47a6b6f66d5
Kyle Jensen <kyle.w.jensen1@gmail.com>
2017-05-27 18:18:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Have_I_Seen {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
