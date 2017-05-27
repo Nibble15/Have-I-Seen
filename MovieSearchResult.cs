9a41e431151bbf1b310f55bb7ea344b6c15273c3
Kyle Jensen <kyle.w.jensen1@gmail.com>
2017-05-27 18:24:35 -0500

    Added MovieSearchResults class with auto properties for get requests on the moviedb api

---
+++ MovieSearchResult.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaveIseen {
+
+    public class RootObject {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string overview { get; set; }
+        public object poster_path { get; set; }
+        public string backdrop_path { get; set; }
+        [JsonProperty(PropertyName = "parts")]
+        public List<MovieSearchResult> MovieSearchResults { get; set; }
+    }
+
+    public class MovieSearchResult {
+        public bool adult { get; set; }
+        public object backdrop_path { get; set; }
+        public int[] genre_ids { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public string overview { get; set; }
+        public string release_date { get; set; }
+        public string poster_path { get; set; }
+        public float popularity { get; set; }
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+        public bool video { get; set; }
+        public float vote_average { get; set; }
+        public int vote_count { get; set; }
+    }
+
+}
